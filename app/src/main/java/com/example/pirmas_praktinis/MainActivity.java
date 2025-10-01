e2bea3c583fbfec209147f28b512a31687317c27
RusneRutkauskaite <rusne.rutkauskaite2@gmail.com>
2025-10-01 14:45:44 +0300

    trecias issaugojimas

--- app/src/main/java/com/example/pirmas_praktinis/MainActivity.java
+++ app/src/main/java/com/example/pirmas_praktinis/MainActivity.java
@@ -5,12 +5,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.graphics.Color; // Reikalinga norint keisti spalvą
 
 public class MainActivity extends AppCompatActivity {
 
-    // Deklaruojame elementus, kad galėtume juos naudoti
+    // Deklaruojame elementus
     private TextView textView;
     private Button button1;
+    private Button button2; // Antras mygtukas
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +24,23 @@ public class MainActivity extends AppCompatActivity {
         // 1. Surandame XML elementus pagal jų ID
         textView = findViewById(R.id.textView);
         button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2); // Surandame antrą mygtuką
 
-        // 2. Nustatome paspaudimo įvykio klausytoją mygtukui
+        // 2. Nustatome paspaudimo įvykio klausytoją (PIRMAS MYGTUKAS: KEIČIA TEKSTĄ)
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // 3. Pakeičiame TextView tekstą, kai mygtukas paspaudžiamas
-                textView.setText("Tekstas pakeistas sėkmingai");
+                // Pakeičiame TextView tekstą, kai mygtukas paspaudžiamas
+                textView.setText("Tekstas pakeistas sėkmingai po mygtuko paspaudimo!");
+            }
+        });
+
+        // 3. Nustatome paspaudimo įvykio klausytoją (ANTRAS MYGTUKAS: KEIČIA SPALVĄ)
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Pakeičiame TextView teksto spalvą į raudoną
+                textView.setTextColor(Color.RED);
             }
         });
     }
